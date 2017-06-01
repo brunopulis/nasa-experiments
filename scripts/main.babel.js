3cffd990e291d167decf9d75af19fd472a50c5be
Bruno Pulis <bruno.pulis@gmail.com>
2017-05-31 22:37:44 -0300

    Finish adjustments to iframe

--- scripts/main.babel.js
+++ scripts/main.babel.js
@@ -1,20 +1,18 @@
-const API_KEY  = 'cnvbe0cPs2FfVtm4BSKqWFxgdKa4i6EOrl8OCsxD'
-const URL      = 'https://api.nasa.gov/planetary/apod?api_key=' + API_KEY
+const API_KEY  = 'cnvbe0cPs2FfVtm4BSKqWFxgdKa4i6EOrl8OCsxD';
+const URL      = 'https://api.nasa.gov/planetary/apod?api_key=' + API_KEY;
 
-let title     = document.querySelector('.title')
-let apod      = document.querySelector('#apod')
-let apodVideo = document.querySelector('#apod-video')
+const apod      = document.querySelector('#apod');
+const apodVideo = document.querySelector('#apod-video');
 
 fetch(URL)
   .then(res => res.json())
   .then(result => {
-    title.innerHTML = result.title
     if( result.media_type == 'video' ) {
-      apod.style.display = 'none'
-      apodVideo.setAttribute('src', result.url)
+      apod.style.display = 'none';
+      apodVideo.setAttribute('src', result.url);
     } else {
-      apod.style.display = 'block'
-      apod.setAttribute('src', result.url)
+      apod.style.display = 'block';
+      apod.setAttribute('src', result.url);
     }
   })
   .catch(err => {
