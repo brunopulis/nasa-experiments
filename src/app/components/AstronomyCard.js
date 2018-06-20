c45e9c97fc12641c3c41bf1b8fae590b2d3f77be
Bruno Pulis <bruno.pulis@gmail.com>
2018-06-20 00:29:48 -0300

    feat(Data Output): Show video when existis

--- src/app/components/AstronomyCard.js
+++ src/app/components/AstronomyCard.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const AstronomyCard = (props) => {
-  const { title, url, hdurl, explanation, date, copyright } = props.data
+  const { title, url, hdurl, explanation, date, copyright, media_type } = props.data;
 
   return(
     <div className="astronomy-card">
       <h2 className="astronomy-title">{ title }</h2>
-
-      <a href={ hdurl } className="astronomy-image-wrapper">
-        <img src={ url } alt={ title } />
-      </a>
-
-      <p>{ explanation }</p>
       <span>{ date }, { copyright } </span>
+      {
+        ( media_type === 'image' ) 
+        ? <img src={ hdurl } className="astronomy-card__img" alt={ title } /> 
+        : <iframe src="{ url }" width="500"  />
+      }
+
+      <div className="astronomy-card__body">
+        <p>{ explanation }</p>
+      </div>
     </div>
   )
 }
 
-export default AstronomyCard
+export default AstronomyCard;
